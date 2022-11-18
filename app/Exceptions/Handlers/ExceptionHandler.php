8eb249874083141c08bd07ee93cb50165f51861e
Eugene Musienko <zdravstvuy1714@gmail.com>
2022-11-18 23:50:24 +0600

    refactoring (framework): prepared framework before development

--- app/Exceptions/Handlers/ExceptionHandler.php
+++ app/Exceptions/Handlers/ExceptionHandler.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace App\Exceptions;
+namespace App\Exceptions\Handlers;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Foundation\Exceptions\Handler;
 use Throwable;
 
-class Handler extends ExceptionHandler
+class ExceptionHandler extends Handler
 {
     /**
      * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
      */
     protected $levels = [
         //
@@ -18,8 +16,6 @@ class Handler extends ExceptionHandler
 
     /**
      * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
         //
@@ -27,8 +23,6 @@ class Handler extends ExceptionHandler
 
     /**
      * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
      */
     protected $dontFlash = [
         'current_password',
@@ -36,12 +30,15 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    protected function shouldReturnJson($request, Throwable $e): bool
+    {
+        return true;
+    }
+
     /**
      * Register the exception handling callbacks for the application.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //

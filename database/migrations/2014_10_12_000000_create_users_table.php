2e1140b0f03c0e7804485b291956bb8d67ba8498
Eugene Musienko <zdravstvuy1714@gmail.com>
2022-11-20 12:46:48 +0600

    fix (conflicts): fixed v2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
 
-            $table->id();
-            $table->string('email')->unique();
-            $table->string('password');
-
             $table->timestamps();
         });
     }
